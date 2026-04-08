3d510fb23957d71f7d8d1526c2de9f60b19eeba2
TechieMini <minakshee7548@gmail.com>
2026-04-08 16:11:22 +0530

    regression model added

--- regression_app.py
+++ regression_app.py
@@ -5,20 +5,20 @@ import pandas as pd
 import streamlit as st
 
 ### Load ANN trained model, scaler pickled file, onehot
-model = load_model('model.h5')
+model = load_model('regression_model.h5')
 
 ### Load the encoder & scaler
-with open('label_encoder_gender.pkl','rb') as file:
+with open('reg_label_encoder_gender.pkl','rb') as file:
     label_encoder_gender = pickle.load(file)
 
-with open('onehot_encoder_geo.pkl','rb') as file:
+with open('reg_onehot_encoder_geo.pkl','rb') as file:
     onehot_encoder_geo = pickle.load(file)
 
-with open('scler.pkl','rb') as file:
+with open('reg_scaler.pkl','rb') as file:
     scaler = pickle.load(file)
 
 ## Streamlit UI creation
-st.title("Customer Churn Prediction")
+st.title("Customer Salary Prediction")
 
 ## Taking user input
 geography = st.selectbox("Geography", onehot_encoder_geo.categories_[0])
@@ -26,11 +26,11 @@ gender = st.selectbox("Gender", label_encoder_gender.classes_)
 age = st.slider("Age", 18, 92)
 balance = st.number_input("Balance")
 credit_score = st.number_input("Credit Score")
-estimated_salary = st.number_input("Estimated Salary")
 tenure = st.slider("Tenure", 0, 10)
 no_of_products = st.slider("No. of Products", 1, 4)
 has_cr_card = st.selectbox("Has Credit Card", [0,1])
 is_active_member = st.selectbox("Is Active member", [0,1])
+exited = st.selectbox("Is Exited", [0,1])
 
 ## Prepare the input data
 # Example input data
@@ -44,7 +44,7 @@ input_data = pd.DataFrame({
     'NumOfProducts': [no_of_products],
     'HasCrCard': [has_cr_card],
     'IsActiveMember': [is_active_member],
-    'EstimatedSalary': [estimated_salary]
+    'Exited': [exited]
 })
 
 ### One-hot encode Geography
@@ -58,11 +58,8 @@ input_scaled = scaler.transform(input_data)
 if st.button("Predict"):
     ## Predict from model
     prediction = model.predict(input_scaled)
-    prediction_probability = prediction[0][0]
-    st.write(f"Chances : {prediction_probability:.2f}")
-    if prediction_probability > 0.5:
-        st.error("Customer is likely to churn")
-    else:
-        st.success("Customer is not likely to churn")
+    prediction_salary = prediction[0][0]
+    st.markdown(f"**Estimated Salary : *{prediction_salary:.2f}***")
+    
 
 # https://ann-classification-churn-techiemini.streamlit.app/
